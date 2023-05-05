7f24b253a61532f94e7d3e3a436baa77e3378d9c
Zeeshan00-7 <za753073@gmail.com>
2023-05-05 06:54:30 +0530

    fetching-post-API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React, { useRef } from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
+export default function App() {
+  const apiUrl = 'https://cdn-api.co-vin.in/api/v2/auth/public/generateOTP';
+
+  const inputRef = useRef(null);
+
+  function mobileOtp() {
+    axios.post(apiUrl, {
+      mobile: parseInt(inputRef.current.value),
+    }, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="number" ref={inputRef} placeholder='number' />
+      <button onClick={mobileOtp}>OTP</button>
     </div>
   );
 }
-
-export default App;
